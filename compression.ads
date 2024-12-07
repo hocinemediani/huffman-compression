a442df148e008beef82df8e71edce6ba566f305b
hocinemediani <dolcejeannarciss@gmail.com>
2024-12-07 19:42:40 +0100

    added contribution to compression_test and changed correct and the order of the symbols

--- compression.ads
+++ compression.ads
@@ -10,34 +10,49 @@ package COMPRESSION is
 	type treeNodeArray is private;
 
    -- Store the sybols used in the text file.
-	procedure GetSymbols (textToCompress : in File_Type; symbolsHashTable : out hashMap);
+	procedure GetSymbols (textToCompress : in File_Type; symbolsHashTable : out hashMap) with
+         Pre => not End_Of_File(textToCompress);
+
    -- This procedure will go through the entire text file and add each character to the symbolsHashTable.
       -- If a character is already inside the hashtable then we need to update the number of occurences by 1.
-         procedure IsInTable (symbol : in Character; symbolsHashTable : in hashMap);
+         procedure IsInTable (symbol : in Character; symbolsHashTable : in hashMap) with 
+               Pre => symbolsHashTable.size > 0;
       -- Else we create the entry.
       -- In the end, we obtain a hash table with each used character and the number of occurences.
       -- The last character is always added and is the ending symbol, with an occurence of 0.
 
 
    -- Build the Huffman tree from the symbols and their number of occurences
-	procedure BuildHuffmanTree (symbolsHashTable: in hashMap; binaryTree : out tree);
+	procedure BuildHuffmanTree (symbolsHashTable: in hashMap; binaryTree : out tree) with
+         Pre => symbolsHashTable.size > 0;
+         Post => binaryTree.root /= Null;
+
    -- This procedure will create the Huffman Tree from the characters present in the text files and their occurences.
       -- First we need to order the indexes of the characters in the hash table from lowest occurence to highest.
-      procedure SortHashTable (symbolsHashTable : in hashMap; sortedHashTable : out hashMap);
+      procedure SortHashTable (symbolsHashTable : in hashMap; sortedHashTable : out hashMap) with
+            Pre => symbolsHashTable.size > 0;
+            Post => sortedHashTable.size > 0;
+
       -- Then at each iterations, we take the two least used characters and create a node.
          -- The resulting node has a nul character, thus indicating that it is only a node with a weight, not characters.
          -- We also keep track of the created sub-trees in the treeArray
          procedure CreateNode (firstNode : in treeNode; secondNode : in treeNode; resultNode : out treeNode; treeArray : out treeNodeArray);
          -- We then put the node in the hash map, where it would be stored if it was a character.
-         procedure PutNode (node : in treeNode; sortedHashTable : in hashMap; fullHashTable : out hashMap);
+         procedure PutNode (node : in treeNode; sortedHashTable : in hashMap; fullHashTable : out hashMap) with
+               Pre => sortedHashTable.size > 0;
+               Post => fullHashTable.size > 0;
+
          -- And we call BuildHuffmanTree again, with the fullHashTable as the first argument.
          -- The iterations stop when we only have one entry in the hash table, with a nul character.
          -- We also need to update the tree structure each time we create a new node (some ndoes will point to another, some will be pointed to)
-         procedure UpdateTree (treeArray : in treeNodeArray; binaryTree : out tree);
-
-
+         procedure UpdateTree (treeArray : in treeNodeArray; binaryTree : out tree) with
+               Post => binaryTree.root /= Null;
+        
    -- Store the encoding of each symbols in a new hash table.
-	procedure GetTextCode (binaryTree : in tree; symbolsHashTable : in hashMap; encodedSymbols : out hashMap);
+	procedure GetTextCode (binaryTree : in tree; symbolsHashTable : in hashMap; encodedSymbols : out hashMap) with
+         Pre => symbolsHashTable.size > 0;
+         Post => encodedSymbols.size = symbolsHashTable.size;
+
    -- This procedure will encode each character by exploring the Huffman tree we created.
       -- So we need to explore the Tree until we have maped each character to a code.
       procedure ExploreTree (binaryTree : in tree; symbol : out Character; code : out String);
@@ -46,18 +61,21 @@ package COMPRESSION is
 
 
 	-- Create the file with the symbols, the tree structure and the encoded text.
-	procedure CreateFile (binaryTree : in tree; encodedSymbols : in hashMap; encodedFile : out File_Type);
+	procedure CreateFile (binaryTree : in tree; encodedSymbols : in hashMap; encodedFile : out File_Type) with
+         Pre => encodedSymbols.size > 0;
+         Post => not End_Of_File(textToCompress);
+
    -- This procedure will create a new file that contains all of the symbols used, the tree structure and the encoded text.
       -- We first need to put in the file every symbols used, sorted by number of occurences.
       procedure PutSymbols (encodedSymbols : in hashMap; encodedFile : out File_Type);
       -- Then we need to find the tree structure by infixed browsing of the tree and put it next to the characters used.
-      procedure InfixBrowsing (binaryTree : in tree; encodedFile : in out File_Type);
+      procedure InfixBrowsing (binaryTree : in tree; encodedFile : in out File_Type; InfixTree : out Unbounded_String);
       -- And finally we need to encode the text.
       -- For that, we iterate through the characters in the raw text file and we store the encoded version of it.
       procedure EncodeText (encodedFile : in out File_Type; encodedSymbols : in hashMap);
 
 
-	
+
 
 private
 
--- compression_test.adb
+++ compression_test.adb
@@ -1,5 +1,6 @@
 with Ada.Text_IO;             use Ada.Text_IO;
 with COMPRESSION;             use COMPRESSION;
+with Ada.Strings.Unbounded;   use Ada.Strings.Unbounded;
 with TH;                      use TH;
 
 procedure COMPRESSION_TEST is
@@ -8,23 +9,30 @@ procedure COMPRESSION_TEST is
    inputFile : File_Type;
    -- A handmade compressed version of the file.
    correctFile : File_Type;
+   correctFileCharacter : Character;
+   outputFileCharacter : Character;
    -- The file compressed.
    outputFile : File_Type;
 
    binaryTree : tree;
    symbolsTable : hashMap;
    encodedSymbols : hashMap;
-
+   encodedFile : File_Type
+   
    -- Used to initialise a hash table with the characters present in the inputFile.
-   arrayLength : CONSTANT Integer := 12;
+   arrayLength : CONSTANT Integer := 11;
    HashTable : hashMap;
 
    Symbols : CONSTANT array (0 .. arrayLength) of Character
-      := ('l', 'a', 'p', 'o', 'm', 'e', 's', 't', 'b', 'n', ' ', '.', "\$");
+      := ('o', 'm', 'n', 'a', 'p', 's', 't', 'b', "\$", 'l', ' ', "e");
    
    Occurences : CONSTANT array (0 .. arrayLength) of Integer
-      := (1, 1, 1, 2, 2, 3, 1, 1, 1, 2, 3, 1, 0);
+      := (2, 2, 2, 1, 1, 1, 1, 1, 0, 1, 3, 3);
 
+   HuffmanCode : CONSTANT array (0 .. arrayLength) of Unbounded_String
+      := ("000", "001", "010", "0110", "0111", "1000", "1001", "1010", "10110", "10111", "110", "111"); 
+   
+   InfixHuffmanTree : CONSTANT Unbounded_String := ("00011010110001101011011");
 
    procedure TestGetSymbols is
    begin
@@ -43,21 +51,34 @@ procedure COMPRESSION_TEST is
 
    procedure TestBuildHuffmanTree is
    begin
-      BuildHuffmanTree (symbolsTable, binaryTree);
-
+      PutSymbols (encodedSymbols , encodedFile);
+      InfixBrowsing (binaryTree, encodedSymbols, infixTree);
+      pragma Assert (infixTree = InfixHuffmanTree);
    end TestBuildHuffmanTree;
 
 
    procedure TestGetTextCode is
    begin
       GetTextCode (binaryTree, symbolsTable, encodedSymbols);
+
+      for j in 0 .. arrayLength loop
+        pragma Assert (ValueOf (encodedSymbols, Symbols(j)) = HuffmanCode(j)); 
+      end loop;
    end TestGetTextCode;
 
 
    procedure TestCreateFile is
    begin
       CreateFile (binaryTree, encodedSymbols, outputFile);
-      
+      Open (correctFile, InFile, "correct.txt");
+      Open (outputFile, Infile, "outputFile.txt");
+      while not End_Of_File (correctFile) and not End_Of_File (outputFile) loop
+        Get (outputFile, outputFileCharacter);
+        Get (correctFile, correctFileCharacter);
+        pragma Assert (outputFileCharacter = correctFileCharacter);
+      end loop;
+      Close (correctFile);
+      Close (outputFile);
    end TestCreateFile;
 
 
