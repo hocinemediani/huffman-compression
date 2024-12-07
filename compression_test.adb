963cc46201c737aad6d65c2ee00b3da4c20401fb
hocinemediani <dolcejeannarciss@gmail.com>
2024-12-07 15:38:02 +0100

    changed correct.txt, still needs the tree structure. made a few changes to the test program

--- compression_test.adb
+++ compression_test.adb
@@ -8,19 +8,22 @@ procedure COMPRESSION_TEST is
    inputFile : File_Type;
    -- A handmade compressed version of the file.
    correctFile : File_Type;
+   -- The file compressed.
+   outputFile : File_Type;
 
-   symbolsTable : hashMap;
    binaryTree : tree;
+   symbolsTable : hashMap;
+   encodedSymbols : hashMap;
 
    -- Used to initialise a hash table with the characters present in the inputFile.
-   arrayLength : CONSTANT Integer := 11;
+   arrayLength : CONSTANT Integer := 12;
    HashTable : hashMap;
 
-   Symbols : CONSTANT array (0 .. 11) of Character
-      := ('l', 'a', 'p', 'o', 'm', 'e', 's', 't', 'b', 'n', ' ', '.');
+   Symbols : CONSTANT array (0 .. arrayLength) of Character
+      := ('l', 'a', 'p', 'o', 'm', 'e', 's', 't', 'b', 'n', ' ', '.', "\$");
    
-   Occurences : CONSTANT array (0 .. 11) of Integer
-      := (1, 1, 1, 2, 2, 3, 1, 1, 1, 2, 3, 1);
+   Occurences : CONSTANT array (0 .. arrayLength) of Integer
+      := (1, 1, 1, 2, 2, 3, 1, 1, 1, 2, 3, 1, 0);
 
 
    procedure TestGetSymbols is
@@ -47,13 +50,14 @@ procedure COMPRESSION_TEST is
 
    procedure TestGetTextCode is
    begin
-
+      GetTextCode (binaryTree, symbolsTable, encodedSymbols);
    end TestGetTextCode;
 
 
    procedure TestCreateFile is
    begin
-
+      CreateFile (binaryTree, encodedSymbols, outputFile);
+      
    end TestCreateFile;
 
 
